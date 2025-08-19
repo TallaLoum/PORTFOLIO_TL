78dbd7f46785bea0f1977d0b7e5b07aa6a7bb8ed
Talla Loum <tallaloum115@gmail.com>
2025-08-20 01:26:23 +0200

    modif

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,14 +1,5 @@
 import React from "react";
-import {
-  Github,
-  ExternalLink,
-  Database,
-  Brain,
-  Code,
-  Server,
-  Globe,
-  Zap,
-} from "lucide-react";
+import { Github, ExternalLink, Database, Brain, Code } from "lucide-react";
 
 const Projects: React.FC = () => {
   const projects = [
